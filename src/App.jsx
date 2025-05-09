6c974f504a940dbc10c2e170ff235c3d81b1a8b6
Shana <shana@Works-MacBook-Pro.local>
2025-05-09 18:36:20 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react';
+import './App.css'; 
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
